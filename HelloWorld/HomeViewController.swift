2a9ea5d2df6b0d4af30bb63dc52ed363efda9680
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-02-18 18:03:30 -0200

    Add the first commit with an initial custom project for Swift.

---
+++ HelloWorld/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Rafael Ferreira on 2/18/16.
+//  Copyright © 2016 Data Empire. All rights reserved.
+//
+
+import UIKit
+
+/** A view controller to home view at storyboard: `Main`. */
+class HomeViewController: UIViewController {
+    
+}
